e8a38a41ba9a33410381fe3c13a521d8746a8afd
kinantatradersfamily <kinanta.tradersfamily@gmail.com>
2025-09-10 20:21:23 +0700

    change admin

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,13 +5,7 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/order', component: () => import('pages/orderItem.vue') },
-    ],
-  },
-  {
-    path: '/admin',                         
-    component: () => import('layouts/MainLayout.vue'), 
-    children: [
-      { path: 'home', component: () => import('pages/admin/home.vue') }
+      { path: '/admin', component: () => import('pages/admin/home.vue') },
     ],
   },
   {
