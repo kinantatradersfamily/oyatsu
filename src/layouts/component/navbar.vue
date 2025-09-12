7251b361b4e361d0ce2f1dd5bc5b7ac053286aca
kinantatradersfamily <kinanta.tradersfamily@gmail.com>
2025-09-12 12:49:39 +0700

    revision

--- src/layouts/component/navbar.vue
+++ src/layouts/component/navbar.vue
@@ -33,7 +33,7 @@
               <a href="" class="user_link">
                 <i class="fa fa-user" aria-hidden="true"></i>
               </a>
-              <a class="cart_link" href="#">
+              <a class="cart_link" href="javascript:void(0)">
                 <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
                   <g>
                     <g>
@@ -86,14 +86,6 @@
                   <g>
                   </g>
                 </svg>
-              </a>
-              <form class="form-inline">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-              <a href="" class="order_online">
-                Order Online
               </a>
             </div>
           </div>
