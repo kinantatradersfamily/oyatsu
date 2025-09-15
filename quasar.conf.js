fa4ccc63493a485f97ef20fde21a3ff553157b5d
kinantatradersfamily <kinanta.tradersfamily@gmail.com>
2025-09-15 14:24:55 +0700

    logo

--- quasar.conf.js
+++ quasar.conf.js
@@ -131,8 +131,8 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Oyatsu`,
+        short_name: `Oyatsu`,
         description: `oyatsuproject`,
         display: 'standalone',
         orientation: 'portrait',
