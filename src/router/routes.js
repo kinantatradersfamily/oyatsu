294d9736bf83b3d56e0f108881be0cb3f4ae845a
kinantatradersfamily <kinanta.tradersfamily@gmail.com>
2025-09-15 14:34:56 +0700

    revision

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/order', component: () => import('pages/orderItem.vue') },
-      { path: '/admin/ninetynine/oyatsu', component: () => import('pages/admin/home.vue') },
+      { path: '/ninetynine-oyatsu-admin', component: () => import('pages/admin/home.vue') },
       { path: '/track', component: () => import('pages/tracking.vue') },
     ],
   },
